38cdda091e4087d55ea9285c4fc73d7134e7f390
Nick Pellegrino <npellegrino@dimagi.com>
2019-08-01 15:32:10 -0400

    rename user_to_next_match to user_to_least_recently_matched

--- matching.py
+++ matching.py
@@ -7,7 +7,7 @@ def match(user_ids, match_history):
 
 
 def get_unmatched_and_next_matches(user_ids, match_history):
-    user_to_next_match = {user: [] for user in user_ids}
+    user_to_least_recently_matched = {user: [] for user in user_ids}
     user_to_unmatched = {user: set(user_ids) - {user} for user in user_ids}
     for historical_match in reversed(match_history):
         user_1 = historical_match[0]
@@ -17,23 +17,23 @@ def get_unmatched_and_next_matches(user_ids, match_history):
         # make a case for de-duplicating here.
         if user_2 in user_to_unmatched[user_1]:
             user_to_unmatched[user_1].remove(user_2)
-            user_to_next_match[user_1].insert(0, user_2)
+            user_to_least_recently_matched[user_1].insert(0, user_2)
         if user_1 in user_to_unmatched[user_2]:
             user_to_unmatched[user_2].remove(user_1)
-            user_to_next_match[user_2].insert(0, user_1)
-    return user_to_unmatched, user_to_next_match
+            user_to_least_recently_matched[user_2].insert(0, user_1)
+    return user_to_unmatched, user_to_least_recently_matched
 
 
-def get_optimal_matches(user_to_unmatched, user_to_next_match):
+def get_optimal_matches(user_to_unmatched, user_to_least_recently_matched):
     if not user_to_unmatched:
         return []
-    users_to_match = set(user_to_next_match)
+    users_to_match = set(user_to_least_recently_matched)
     # break ties by user id for deterministic tests
     # TODO - refactor this into helper function and use Python's mock library to guarantee
     # deterministic sorting.  In production, we might not need this function to be deterministic,
     # and providing a second sort key (user) adds overhead.
-    users_by_most_matched = sorted(user_to_next_match,
-        key=lambda user: (len(user_to_next_match[user]), user), reverse=True)
+    users_by_most_matched = sorted(user_to_least_recently_matched,
+        key=lambda user: (len(user_to_least_recently_matched[user]), user), reverse=True)
 
     next_user_to_match = users_by_most_matched[0]
     users_to_match.remove(next_user_to_match)
@@ -41,14 +41,14 @@ def get_optimal_matches(user_to_unmatched, user_to_next_match):
     if unmatched_users:
         matched_user = next(filter(lambda user: user in unmatched_users, users_by_most_matched))
     else:
-        matched_user = next(filter(lambda user: user in users_to_match, user_to_next_match[next_user_to_match]))
+        matched_user = next(filter(lambda user: user in users_to_match, user_to_least_recently_matched[next_user_to_match]))
 
     next_user_to_unmatched = {
         user: {x for x in user_to_unmatched[user] if x not in [next_user_to_match, matched_user]} for user in user_to_unmatched
         if user not in [next_user_to_match, matched_user]
     }
     next_user_to_next_match = {
-        user: [x for x in user_to_next_match[user] if x not in [next_user_to_match, matched_user]] for user in user_to_next_match
+        user: [x for x in user_to_least_recently_matched[user] if x not in [next_user_to_match, matched_user]] for user in user_to_least_recently_matched
         if user not in [next_user_to_match, matched_user]
     }
     return [(next_user_to_match, matched_user)] + get_optimal_matches(next_user_to_unmatched, next_user_to_next_match)
